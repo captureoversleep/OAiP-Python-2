a6bf8c0d2948ca80cf7f2dbfeb56a9c617493020
captureoversleep <asdweer@list.ru>
2026-09-08 14:51:28 +0000

    improved

--- 3.py
+++ 3.py
@@ -1,6 +1,7 @@
 feedback = 'В отеле "Либерти" мне больше всего понравился заботливый персонал'
 feedback = feedback.lower()
-#pos1 = feedback.find('заботлиывй')
-#print(pos1)
-fb1 = feedback[46:65]
-print(fb1)
+target_lenght = len('заботливый персонал')
+pos = feedback.find('заботливый')
+
+fb_out = feedback[pos:pos + target_lenght]
+print(fb_out)
