6f5e356380475a83c29e0ca3141c857c1d3266a6
captureoversleep <asdweer@list.ru>
2026-09-08 15:31:31 +0500

    Add files via upload
    task 1

---
+++ 1.py
@@ -0,0 +1,7 @@
+dish = input('Введите понравившиеся блюда: ')
+feedback = dish
+feedback = feedback.lower()
+position2 = feedback.find('шашлык')
+position1 = feedback.find('шоколадный торт')
+print(position1)
+print(position2)
