7814d15e008264e50ad141bba1747a5bc1b03cff
captureoversleep <asdweer@list.ru>
2026-09-08 15:40:15 +0500

    Add files via upload
    task 3

---
+++ 3.py
@@ -0,0 +1,6 @@
+feedback = 'В отеле "Либерти" мне больше всего понравился заботливый персонал'
+feedback = feedback.lower()
+#pos1 = feedback.find('заботлиывй')
+#print(pos1)
+fb1 = feedback[46:65]
+print(fb1)
